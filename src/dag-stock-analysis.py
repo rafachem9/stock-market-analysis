60532d6d2521fcc4d91c683eb1058a4fad089aec
Rafael Ramirez <rafa.ramirez.9@gmail.com>
2025-12-01 22:38:54 +0000

    feat: modify SendOperator

--- src/dag-stock-analysis.py
+++ src/dag-stock-analysis.py
@@ -4,9 +4,12 @@ from airflow.operators.empty import EmptyOperator
 from datetime import datetime
 from airflow.operators.email import EmailOperator
 
+# -----------------------------
+# settings
+# -----------------------------
 
-FILE_PATH = "/home/rafachem9/data-engineer/stock-market-analysis/src/"
-
+FILE_PATH = "/home/rafachem9/data-engineer/stock-market-analysis"
+EMAIL_LIST = ["rafachem9@gmail.com", "rafachem9@gmail.com"]
 # -----------------------------
 # DAG settings
 # -----------------------------
@@ -14,6 +17,8 @@ default_args = {
     'owner': 'rafachem9',
     'depends_on_past': False,
     'retries': 1,
+    "email": ["rafachem9@gmail.com"],
+    "email_on_failure": True,
 }
 
 dag = DAG(
@@ -39,20 +44,33 @@ start = EmptyOperator(
 # -----------------------------
 run_script = BashOperator(
     task_id='execute_main_py',
-    bash_command=f'python3 {FILE_PATH}main.py',
+    bash_command=f'python3 {FILE_PATH}/src/main.py',
     dag=dag
 )
 
 send_email = EmailOperator(
     task_id="send_email",
-    to="rafa.ramirez.9@gmail.com",
-    subject="Reporte diario con adjunto",
+    to=EMAIL_LIST,
+    subject="Reporte diario con adjunto {{ ds }}",
     html_content="""
     <h3>Hola,</h3>
     <p>Adjunto el fichero con el reporte diario.</p>
+    <img src="cid:sp500_volatility_2025.png" alt="Logo" style="width:200px;">
+    <img src="cid:etf_return_2025.png" alt="Logo" style="width:200px;">
+    <img src="cid:ibex_35_volatility_2025.png" alt="Logo" style="width:200px;">
+
     """,
-    files=[FILE_PATH],
+    files=[
+        f"{FILE_PATH}/data/sp500_analysed_df.csv",
+                        f"{FILE_PATH}/data/dividendos_ibex35_analysed_df.csv",
+                        f"{FILE_PATH}/data/dividendos_sp500_analysed_df.csv",
+                        f"{FILE_PATH}/data/sp500_volatility_2025.png",
+                        f"{FILE_PATH}/data/etf_return_2025.png",
+                        f"{FILE_PATH}/data/ibex_35_volatility_2025.png",
+                        f"{FILE_PATH}/data/ibex35_analysed_df.csv"
+    ],
     conn_id="my_smtp_connection",
+    mime_subtype='related',                  # Necesario para manejar multipart MIME
 )
 
 
@@ -67,5 +85,5 @@ end = EmptyOperator(
 # -----------------------------
 # Task dependencies
 # -----------------------------
-start >> run_script >> end
+start >> run_script >> send_email >> end
 
